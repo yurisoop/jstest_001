8e8e5218a1d473a720caa6601f56d80aea60f4ce
Ye Seung Lee <yurisoop@gmail.com>
2018-11-20 12:16:36 +0900

    테스트 코드 작성

---
+++ Sample/Sample.js
@@ -0,0 +1,37 @@
+// let name =  "홍길동"
+// let age = 30;
+// console.log(age);
+
+let name = "YeSeung";
+let len = name.length;
+console.log(len);
+
+let newName = name.replace('Ye', 'Sam');
+console.log(newName);
+
+let char = newName.charAt(2);
+console.log(char);
+
+console.log(newName.toUpperCase());
+console.log(newName.toLowerCase());
+
+var x = 0;
+x += 5;
+console.log(x);
+
+console. log(x);
+console.log('hello' + 'world');
+console.log('1' + 2+3+4);
+
+console.log(123 == '123');
+
+
+
+
+
+
+
+
+
+
+
